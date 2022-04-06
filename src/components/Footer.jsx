1c7a92d4f6e810452a4de3eedb43b7eee7c1acb7
Pradumn Kumar <kumarpradumn2000@gmail.com>
2022-04-06 20:18:50 +0530

    Initial

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+var curr_year = new Date().getFullYear();
+
+function Footer() {
+  return (
+    <footer>
+      <p>© Copyright Pradumn Kumar {currYear}</p>
+    </footer>
+  );
+}
+
+export default Footer;
