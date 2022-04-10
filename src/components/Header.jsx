ac575189566d0eb3771b747159d0c1c014652073
Pradumn Kumar <kumarpradumn2000@gmail.com>
2022-04-10 11:31:30 +0530

    Updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function Header(){
 
     return (
-    <header>
-    <h1 >Keeper</h1>
-    </header>
+        <header>
+        <h1 >Keeper</h1>
+        </header>
     );
 }
 
